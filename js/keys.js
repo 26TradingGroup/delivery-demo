b2a011ea1051be5b88280cf377b1bd8ec48e4869
26TradingGroup <136130062+26TradingGroup@users.noreply.github.com>
2023-06-15 16:17:01 -0500

    Update keys.js

--- js/keys.js
+++ js/keys.js
@@ -3,7 +3,7 @@
  * Please see the ReadMe file for details of where to obtain keys
  */
 
-const publish_key = '' //  ENTER YOUR PUBLISH KEY HERE
-const subscribe_key = '' //  ENTER YOUR SUBSCRIBE KEY HERE
+const publish_key = '' //  pub-c-6305f388-8584-47e7-8898-4974cacd2283
+const subscribe_key = '' //  sub-c-ee2d2a29-a0e7-41f7-9fe4-b952ad347d09
 
-const GOOGLE_KEY = '' //  ENTER GOOGLE API KEY WITH THE FOLLOWING APIS ENABLED: JAVASCRIPT MAPS SDK, GEOCODING API, DIRECTIONS API.
+const GOOGLE_KEY = '' //  AIzaSyALvWMir2ujAIfk0I5LwufOVx-j_DhES3I.
