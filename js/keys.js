415df778f39822c202299e5e8f635cfdfee07b83
26TradingGroup <136130062+26TradingGroup@users.noreply.github.com>
2023-06-19 12:14:27 -0500

    Update keys.js

--- js/keys.js
+++ js/keys.js
@@ -3,7 +3,6 @@
  * Please see the ReadMe file for details of where to obtain keys
  */
 
-const publish_key = '' //  pub-c-6305f388-8584-47e7-8898-4974cacd2283
-const subscribe_key = '' //  sub-c-ee2d2a29-a0e7-41f7-9fe4-b952ad347d09
-
-const GOOGLE_KEY = '' //  AIzaSyALvWMir2ujAIfk0I5LwufOVx-j_DhES3I.
+const publish_key = '' //  ENTER PUBLISH_KEY HERE //
+const subscribe_key = '' // ENTER SUBSCRIBE_KEY HERE //
+const GOOGLE_KEY = '' // ENTER GOOGLE_KEY HERE//
